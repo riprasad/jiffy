49da7c92a2199d09220d798ab32c37982d79911f
kumarVikram <kumarvikramshahi@gmail.com>
2023-07-28 14:08:11 +0530

    feat: user can store vars in config file

--- cmd/jiffy/root.go
+++ cmd/jiffy/root.go
@@ -5,10 +5,10 @@ import (
 	"os"
 	"text/tabwriter"
 
+	"github.com/riprasad/jiffy/pkg/config"
 	"github.com/riprasad/jiffy/pkg/cve"
 	"github.com/riprasad/jiffy/pkg/issue"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -20,31 +20,43 @@ var rootCmd = &cobra.Command{
 	},
 }
 
+// will show CVE infos like BugzillaID, JiraKey, AffectedPackage, Summary and ID
 var Info = &cobra.Command{
-	Use:   "Info",
+	Use:   "info",
 	Short: "Print CVEs info of passed token and jql",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("in info section start")
-		viper.SetConfigFile(".env")
-		viper.ReadInConfig()
+		cfg := config.GetConfiguration()
+		if cfg.JiraToken == "" || cfg.Jql == "" {
+			fmt.Println(`Either Jira Token or Jql is empty. Please pass both values in "config.yaml" to continue :)`)
+			os.Exit(1)
+		}
 
-		token := viper.Get("token").(string)
-		jql := viper.Get("jql").(string)
+		token := cfg.JiraToken
+		jql := cfg.Jql
 		issues := issue.GetInfo(token, jql)
 		cves := cve.CurateCveDetails(issues)
 
 		// print in proper table form on stdout
 		w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.TabIndent)
-		fmt.Fprintf(w, "%s\t%s\t\n", "BugzillaID", "JiraKey")
+		fmt.Fprintf(w, "%s\t%s\t\n", "Bugzilla_ID", "Jira_Key")
 		for _, cveInfo := range cves {
 			fmt.Printf("%s %s ", cveInfo.BugzillaID, cveInfo.JiraKey)
+		}
+		w.Flush()
+
+		fmt.Println()
+		fmt.Println()
+
+		// print in proper table form on stdout
+		w = tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.TabIndent)
+		fmt.Fprintf(w, "%s\t%s\t%s\t\n", "ID", "Affected_Package", "Summary")
+		for _, cveInfo := range cves {
+			fmt.Printf("%s %s %s", cveInfo.ID, cveInfo.AffectedPackage, cveInfo.Summary)
 		}
 		w.Flush()
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -53,14 +65,7 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.jiffy.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	config.InitConfiguration()
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	rootCmd.AddCommand(Info)
 }
